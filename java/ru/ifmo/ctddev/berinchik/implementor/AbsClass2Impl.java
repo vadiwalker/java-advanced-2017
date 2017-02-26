6ffc9e89290c251173b262110d511872873eedeb
vadiwalker <berinchik2012@gmail.com>
2017-02-26 23:45:35 +0300

    useless classes deleted

--- java/ru/ifmo/ctddev/berinchik/implementor/AbsClass2Impl.java
+++
@@ -1,6 +0,0 @@
-package ru.ifmo.ctddev.berinchik.implementor;
-import java.lang.String;
-@SuppressWarnings("unchecked")
-@Deprecated
-public class AbsClass2Impl extends AbsClass2{ protected AbsClass2Impl(int a){ super(a); }
-}
--- java/ru/ifmo/ctddev/berinchik/implementor/BaseImpl.java
+++
@@ -1,9 +0,0 @@
-package ru.ifmo.ctddev.berinchik.implementor;
-import java.lang.annotation.Annotation;
-public class BaseImpl implements Base {
-	@Override
-	public int f() {
-		return 42;
-	}
-	
-}
